b3dc521e01c617b750df8a3bb966f8768b06370a
Mateusz Szumichora <mateuszszumichora@gmail.com>
2019-09-11 14:04:29 +0200

    Start projektu z create-react-app

---
+++ src/app/layout/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Treningowa Apka</h1>
+        <p>Nie masz z kim trenować ? Z naszą apką to już nie problem</p>
+      </div>
+    )
+  }
+}
+
+export default App
